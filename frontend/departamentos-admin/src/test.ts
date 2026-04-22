efa3253211a735058279148e53c4c6d55f351c3f
Erick <romerogarciaerick85@gmail.com>
2026-04-21 21:26:47 -0600

    sonnar

---
+++ frontend/departamentos-admin/src/test.ts
@@ -0,0 +1,13 @@
+/// <reference types="jasmine" />
+import 'zone.js/testing';
+import { getTestBed } from '@angular/core/testing';
+import {
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting
+} from '@angular/platform-browser-dynamic/testing';
+
+// Inicializa entorno de pruebas
+getTestBed().initTestEnvironment(
+  BrowserDynamicTestingModule,
+  platformBrowserDynamicTesting(),
+);
